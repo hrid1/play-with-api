e8ccd38bc13582c90e1f32c16f7b8027730f8852
hrid1 <hridoycse1@gmail.com>
2024-02-03 23:42:32 +0600

    phone hunter 80 percent

---
+++ phone-hunter/main.js
@@ -0,0 +1,56 @@
+const loadPhone = phone => {
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${phone}`)
+    .then(res => res.json())
+    .then(info => displayPhone(info.data))
+}
+
+const phoneContainer = document.getElementById('phone-div');
+
+const displayPhone = info => {
+    phoneContainer.innerHTML = '';
+    // display 10 phones only
+    info = info.slice(0, 10)
+
+    // display no phone found
+    const noPhone = document.getElementById("no-found-msg");
+    if (info.length === 0){
+        noPhone.classList.remove('d-none');
+    }
+    else {
+        noPhone.classList.add('d-none');
+    }
+
+
+    // loop start here
+    for(let item of info){
+       
+        const newPhone = document.createElement('div');
+        newPhone.classList.add('col');
+        newPhone.innerHTML = `
+
+        <div class="card">
+            <img src="${item.image}" class="card-img-top img-fluid p-1" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${item.phone_name}</h5>
+                <p class="card-text">This is a longer card with supporting text below a longer.</p>
+            </div>
+        </div>
+
+        `
+        phoneContainer.appendChild(newPhone);
+   
+    }
+}
+
+
+// get the search data 
+const searchPhone = () => {
+    const phoneName = document.getElementById("search-box").value;
+    loadPhone(phoneName);
+}
+
+
+
+
+
+
---
+++ phone-hunter/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+const searchPhone = () => {
+    const phoneName = document.getElementById("search-box").value;
+    console.log(phoneName)
+}
+searchPhone()
