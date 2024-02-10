320434d08e5791cf6296b2c076844ca544657ff6
hrid1 <hridoycse1@gmail.com>
2024-02-11 00:35:16 +0600

    sport db done

--- sports-db/script.js
+++ sports-db/script.js
@@ -16,14 +16,21 @@ const loadPlayer = () => {
 
 const displayPlayer = data => {
 
+    // if(data.length === 0 ) {
+    //     document.getElementById('player-found').classList.remove('d-none');
+      
+    // }
+    // else {
+    //     document.getElementById('player-found').classList.add('d-none');
+    // }
+
     const playerDiv = document.getElementById('player-div');
     playerDiv.innerHTML = '';
 
     data.forEach(player => {
-        // console.log(player.strPlayer)
-        // console.log(player)
-        const { strThumb, strPlayer, strNationality} = player;
-        console.log(strPlayer, strThumb, strNationality);
+        
+        const { strThumb, strPlayer, strNationality, idPlayer} = player;
+        // console.log(strPlayer, strThumb, strNationality, idPlayer);
 
         const newPlayer = document.createElement('div');
         newPlayer.classList.add('col');
@@ -38,17 +45,54 @@ const displayPlayer = data => {
 
         <div class="text-center">
 
-        <button type="button" class="btn btn-info mx-4 my-3">Info</button>
+        <button type="button" onclick="loadSinglePlayer('${idPlayer}')" class="btn btn-info mx-4 my-3">Details</button>
         <button type="button" class="btn btn-dark mx-4 my-2">Delete</button>
 
         </div>
 
         `
         playerDiv.appendChild(newPlayer);
+
+        const info = document.getElementById('single-player-info');
+        info.classList.add('d-none')
     });
+}
 
+
+const loadSinglePlayer = (idPlayer) =>{
+    const URL = `https://www.thesportsdb.com/api/v1/json/3/lookupplayer.php?id=${idPlayer}`
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => displayPlayerDetails(data));
     
+}
 
+
+const displayPlayerDetails = data => {
+
+    const info = document.getElementById('single-player-info');
+    info.classList.remove('d-none')
+   
+    const playerDetails = data.players[0];
+    const {strThumb, strPlayer, strNationality, strGender
+    } = playerDetails;
+    // console.log(strThumb, strPlayer, strNationality);
+    console.log(playerDetails);
+
+   document.getElementById('single-img').src = strThumb;
+   document.getElementById('single-name').innerText = strPlayer;
+   document.getElementById('single-country').innerText = strNationality;
+
+   const teamImg =  document.getElementById('team-img');
+
+   if(strGender == 'Male') {
+        teamImg.src = 'https://pbs.twimg.com/media/FE0I7-bWUAcn5Rc.jpg:large'
+   }
+   else {
+        teamImg.src = 'https://top-mmo.fr/wp-content/uploads/2022/04/FIFA-Best-Women-s-Player-2021-nominees.jpeg';
+   }
 
 }
+
+
 
